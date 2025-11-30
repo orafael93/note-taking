63336b2328fde784eef1d2b0e3f425a52eb90bbe
Rafael Pereira <orafaeldev@gmail.com>
2025-11-30 16:24:28 -0300

    feat: add logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import { LogoIcon } from "../Icons";
+
+import * as S from "./styles";
+
+export const Logo = () => (
+  <S.Wrapper>
+    <LogoIcon />
+  </S.Wrapper>
+);
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  @media (max-width: 1024px) {
+    padding: 20px 30px;
+    background-color: #232530;
+  }
+`;
