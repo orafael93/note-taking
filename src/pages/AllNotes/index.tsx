f25d0998d35cd21a5d49c6f0bafcf79e0ac0088a
Rafael Pereira <orafaeldev@gmail.com>
2025-12-06 20:15:18 -0300

    refactor: refactor all notes page

--- src/pages/AllNotes/index.tsx
+++ src/pages/AllNotes/index.tsx
@@ -1,45 +1,100 @@
-import { Plus } from "lucide-react";
+import { Fragment, useRef } from "react";
+import { Plus, Settings } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
+import { Logo } from "@/components/Logo";
 import { NoteCard } from "@/components/Note/NoteCard";
+import { NoteDetail } from "@/components/NoteDetail";
 import { SearchInput } from "@/components/SearchInput";
 import { useNotesStore } from "@/store/notes";
 
-import * as Types from "./types";
 import * as S from "./styles";
 
-export const AllNotes = (props: Types.AllNotesType) => {
-  const isSearchingNotes = useNotesStore((store) => store.isSearchingNotes);
+export const AllNotes = () => {
+  const navigate = useNavigate();
 
-  const { onNoteSelect } = props;
+  const isSearchingNotes = useNotesStore((store) => store.isSearchingNotes);
+  const selectedNoteId = useNotesStore((state) => state.selectedNoteId);
+  const onUpdateSelectedNote = useNotesStore(
+    (state) => state.onUpdateSelectedNote
+  );
+
+  const contentWrapperRef = useRef<HTMLDivElement | null>(null);
+
+  const handleSelectNote = (noteId: string) => {
+    contentWrapperRef.current?.scrollTo({
+      top: 0,
+    });
+
+    onUpdateSelectedNote(noteId);
+  };
+
+  const handleCloseNote = () => {
+    onUpdateSelectedNote(null);
+  };
 
   const notes = useNotesStore((state) =>
     state.notes.filter((note) => !note.isArchived)
   );
 
   return (
-    <S.Container>
-      <S.CreateButton style={{ marginBottom: "16px" }}>
-        <Plus size={16} strokeWidth={3} />
-        Create New Note
-      </S.CreateButton>
+    <Fragment>
+      <S.MainContent>
+        <S.LogoWrapper>
+          <Logo />
+        </S.LogoWrapper>
 
-      {isSearchingNotes ? null : (
-        <S.TitleWrapper>
-          <S.Title>All Notes</S.Title>
-        </S.TitleWrapper>
-      )}
+        <S.MainHeaderWrapper>
+          <S.Header>
+            <S.Title>All Notes</S.Title>
+          </S.Header>
 
-      {isSearchingNotes && <SearchInput />}
+          <S.SearchInputAndSettingsIconWrapper>
+            <SearchInput />
 
-      <S.NotesList>
-        {notes.map((note) => (
-          <NoteCard
-            key={note.title}
-            note={note}
-            onClick={() => onNoteSelect(note.title)}
-          />
-        ))}
-      </S.NotesList>
-    </S.Container>
+            <S.SettingsIconWrapper onClick={() => navigate("/settings")}>
+              <Settings size={20} style={{ cursor: "pointer" }} />
+            </S.SettingsIconWrapper>
+          </S.SearchInputAndSettingsIconWrapper>
+        </S.MainHeaderWrapper>
+
+        <S.ContentWrapper ref={contentWrapperRef}>
+          {selectedNoteId && window.innerWidth <= 1024 ? null : (
+            <S.Container>
+              <S.CreateButton style={{ marginBottom: "16px" }}>
+                <Plus size={16} strokeWidth={3} />
+                Create New Note
+              </S.CreateButton>
+
+              {isSearchingNotes ? null : (
+                <S.TitleWrapper>
+                  <S.Title>All Notes</S.Title>
+                </S.TitleWrapper>
+              )}
+
+              {isSearchingNotes && <SearchInput />}
+
+              <S.NotesList>
+                {notes.map((note) => (
+                  <NoteCard
+                    key={note.title}
+                    note={note}
+                    onClick={() => handleSelectNote(note.title)}
+                  />
+                ))}
+              </S.NotesList>
+            </S.Container>
+          )}
+
+          {selectedNoteId && (
+            <NoteDetail
+              noteId={selectedNoteId}
+              onBack={handleCloseNote}
+              key={selectedNoteId}
+            />
+          )}
+        </S.ContentWrapper>
+      </S.MainContent>
+    </Fragment>
   );
 };
--- src/pages/AllNotes/styles.ts
+++ src/pages/AllNotes/styles.ts
@@ -14,7 +14,7 @@ export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: ${({ theme }) => theme.spacing[200]};
+
   border-bottom: 1px solid ${({ theme }) => theme.colors.neutral[800]};
 `;
 
@@ -74,5 +74,83 @@ export const NotesList = styled.div`
 
   & > div:last-child {
     border-bottom: none;
+  }
+`;
+
+export const MainContent = styled.main`
+  position: relative;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr;
+
+  @media (max-width: 1024px) {
+    height: calc(100dvh - 64px);
+    overflow: hidden;
+
+    grid-column: 1 / -1;
+  }
+`;
+
+export const LogoWrapper = styled.div`
+  display: none;
+
+  @media (max-width: 1024px) {
+    display: block;
+  }
+`;
+
+export const MainHeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 20px 30px;
+
+  min-height: 81px;
+
+  border-bottom: 2px solid #525866;
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const AllNotesHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border-bottom: 1px solid ${({ theme }) => theme.colors.neutral[800]};
+`;
+
+export const SearchInputAndSettingsIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.75rem;
+`;
+
+export const SettingsIconWrapper = styled.div`
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 290px 1fr;
+
+  border-left: 1px solid ${({ theme }) => theme.colors.neutral[800]};
+  border-right: 1px solid ${({ theme }) => theme.colors.neutral[800]};
+
+  overflow-y: auto;
+
+  height: 100%;
+
+  @media (max-width: 1024px) {
+    height: calc(100dvh - 128px);
+    border: none;
+
+    grid-template-columns: 1fr;
   }
 `;
