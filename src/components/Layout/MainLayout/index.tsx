f82f154dbddc83fd393ade6798d0bad90c7834eb
Rafael Pereira <orafaeldev@gmail.com>
2025-12-06 20:11:00 -0300

    feat: add app routes

--- src/components/Layout/MainLayout/index.tsx
+++ src/components/Layout/MainLayout/index.tsx
@@ -1,73 +1,26 @@
-import { useRef } from "react";
-import { Settings } from "lucide-react";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-import { Logo } from "@/components/Logo";
 import { Sidebar } from "@/components/Layout/Sidebar";
 import { AllNotes } from "@/pages/AllNotes";
-import { NoteDetail } from "@/pages/NoteDetail";
-import { SearchInput } from "@/components/SearchInput";
-import { useNotesStore } from "@/store/notes";
+import { Settings } from "@/pages/Settings";
+import { ArchivedNotes } from "@/pages/ArchivedNotes";
 import { BottomNavigation } from "@/components/Navigation/BottomNavigation";
 
 import * as S from "./styles";
 
-export const MainLayout = () => {
-  const selectedNoteId = useNotesStore((state) => state.selectedNoteId);
-  const onUpdateSelectedNote = useNotesStore(
-    (state) => state.onUpdateSelectedNote
-  );
-
-  const contentWrapperRef = useRef<HTMLDivElement | null>(null);
-
-  const handleSelectNote = (noteId: string) => {
-    contentWrapperRef.current?.scrollTo({
-      top: 0,
-    });
-
-    onUpdateSelectedNote(noteId);
-  };
-
-  const handleCloseNote = () => {
-    onUpdateSelectedNote(null);
-  };
-
-  return (
-    <S.Container>
+export const MainLayout = () => (
+  <S.Container>
+    <BrowserRouter>
       <Sidebar />
-      <S.MainContent>
-        <S.LogoWrapper>
-          <Logo />
-        </S.LogoWrapper>
 
-        <S.MainHeaderWrapper>
-          <S.Header>
-            <S.Title>All Notes</S.Title>
-          </S.Header>
+      <Routes>
+        <Route path="/" element={<AllNotes />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/archived" element={<ArchivedNotes />} />
+        <Route path="*?" element={<AllNotes />} />
+      </Routes>
+    </BrowserRouter>
 
-          <S.SearchInputAndSettingsIconWrapper>
-            <SearchInput />
-
-            <S.SettingsIconWrapper>
-              <Settings size={20} style={{ cursor: "pointer" }} />
-            </S.SettingsIconWrapper>
-          </S.SearchInputAndSettingsIconWrapper>
-        </S.MainHeaderWrapper>
-
-        <S.ContentWrapper ref={contentWrapperRef}>
-          {selectedNoteId && window.innerWidth <= 1024 ? null : (
-            <AllNotes onNoteSelect={handleSelectNote} />
-          )}
-
-          {selectedNoteId && (
-            <NoteDetail
-              noteId={selectedNoteId}
-              onBack={handleCloseNote}
-              key={selectedNoteId}
-            />
-          )}
-        </S.ContentWrapper>
-      </S.MainContent>
-      <BottomNavigation />
-    </S.Container>
-  );
-};
+    <BottomNavigation />
+  </S.Container>
+);
