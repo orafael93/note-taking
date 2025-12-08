b271eca6ac67a468a0d74c68b5acfd3a36724c1c
Rafael Pereira <orafaeldev@gmail.com>
2025-12-08 18:13:52 -0300

    feat: add create account route

--- src/components/Layout/MainLayout/index.tsx
+++ src/components/Layout/MainLayout/index.tsx
@@ -5,6 +5,7 @@ import { Sidebar } from "@/components/Layout/Sidebar";
 import { AllNotes } from "@/pages/AllNotes";
 import { Settings } from "@/pages/Settings";
 import { ArchivedNotes } from "@/pages/ArchivedNotes";
+import { CreateAccount } from "@/pages/CreateAccount";
 import { ResetPassword } from "@/pages/ResetPassword";
 import { ForgotPassword } from "@/pages/ForgotPassword";
 import { BottomNavigation } from "@/components/Navigation/BottomNavigation";
@@ -21,6 +22,7 @@ export const MainLayout = () => (
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/archived" element={<ArchivedNotes />} />
         <Route path="*?" element={<AllNotes />} />
