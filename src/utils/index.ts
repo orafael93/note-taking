92a5d1041a3f1f98f6784bc2d74a1e6888e12463
Rafael Pereira <orafaeldev@gmail.com>
2025-12-15 22:18:16 -0300

    feat: add initProjectTheme util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export const initProjectFont = () => {
   document.documentElement.setAttribute("data-font", "sans-serif");
 };
+
+export const initProjectTheme = () => {
+  document.documentElement.setAttribute("data-theme", "light");
+};
