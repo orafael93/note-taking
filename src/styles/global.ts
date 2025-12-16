d6ff6087cbbc5c97d5df2330223277f7ead78a3f
Rafael Pereira <orafaeldev@gmail.com>
2025-12-15 22:09:18 -0300

    feat: add theme colors

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -77,6 +77,48 @@ export const GlobalStyle = createGlobalStyle`
 
   :root {
     --current-font: 'Inter', sans-serif;
+
+    --color-base: #000;
+
+    --color-neutral-0: #FFFFFF;
+    --color-neutral-50: #F5F7FA;
+    --color-neutral-100: #F3F5F8;
+    --color-neutral-200: #E0E4EA;
+    --color-neutral-300: #CACFD8;
+    --color-neutral-400: #99A0AE;
+    --color-neutral-500: #717784;
+    --color-neutral-600: #525866;
+    --color-neutral-700: #28303B;
+    --color-neutral-800: #232530;
+    --color-neutral-900: #191825;
+    --color-neutral-950: #0E1218;
+
+    --color-blue-50: #EBF1FF;
+    --color-blue-500: #335CFF;
+    --color-blue-700: #2547D0;
+
+    --color-green-100: #21C16B;
+    --color-green-500: #D1FBE9;
+
+    --color-red-100: #FFD5D8;
+    --color-red-500: #FB374B;
+  }
+
+  [data-theme="dark"] {
+    --color-base: #fff;
+
+    --color-neutral-0: #0E1218;
+    --color-neutral-50: #191825;
+    --color-neutral-100: #232530;
+    --color-neutral-200: #28303B;
+    --color-neutral-300: #525866;
+    --color-neutral-400: #717784;
+    --color-neutral-500: #99A0AE;
+    --color-neutral-600: #CACFD8;
+    --color-neutral-700: #E0E4EA;
+    --color-neutral-800: #F3F5F8;
+    --color-neutral-900: #F5F7FA;
+    --color-neutral-950: #FFFFFF;
   }
 
   [data-font="serif"] {
@@ -96,8 +138,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: ${({ theme }) => theme.colors.neutral[900]};
-    color: ${({ theme }) => theme.colors.neutral[100]};
+    background-color: var(--color-neutral-0);
     -webkit-font-smoothing: antialiased;
   }
 
