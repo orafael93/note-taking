0884de23794461b743e792312b53618ecf156ee0
Rafael Pereira <orafaeldev@gmail.com>
2025-12-16 20:38:44 -0300

    refactor: change ForgotPassword page colors to support light and dark theme

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -10,16 +10,27 @@ export default () => {
         <div style={{ textAlign: "center" }}>
           <Logo />
         </div>
-
         <h1
-          style={{ textAlign: "center", marginTop: "16px", fontSize: "24px" }}
+          style={{
+            textAlign: "center",
+            marginTop: "16px",
+            fontSize: "24px",
+            color: "var(--color-neutral-950)",
+          }}
         >
           Forgotten your password?
         </h1>
-        <h2 style={{ textAlign: "center", fontSize: "14px", marginTop: "8px" }}>
+        <h2
+          style={{
+            textAlign: "center",
+            fontSize: "14px",
+            marginTop: "8px",
+            fontWeight: 500,
+            color: "var(--color-neutral-600)",
+          }}
+        >
           Enter your email below, and we'll send you a link to reset it.
         </h2>
-
         <div
           style={{
             marginTop: "50px",
@@ -27,7 +38,6 @@ export default () => {
         >
           <Input label="Email address" id="email" type="email" />
         </div>
-
         <S.Button style={{ marginTop: "1rem" }}>Send Reset Link</S.Button>
       </S.Content>
     </S.Wrapper>
