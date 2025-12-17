aedf6a1c8d5ae913f1d83ad80f6616e926eb2ddd
Rafael Pereira <orafaeldev@gmail.com>
2025-12-17 20:02:29 -0300

    feat: add types to CreateNote component

--- src/components/CreateNote/types.ts
+++ src/components/CreateNote/types.ts
@@ -1,3 +1,10 @@
 export type CreateNoteType = {
   onBack: () => void;
 };
+
+export type FieldsType = "title" | "tags" | "content";
+
+export type OnUpdateNoteContentType = {
+  key: FieldsType;
+  value: string;
+};
