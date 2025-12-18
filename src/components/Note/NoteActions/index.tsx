923c19bf0e43a922eb53eef4f90db1285e4931b1
Rafael Pereira <orafaeldev@gmail.com>
2025-12-18 20:14:46 -0300

    refactor: refactor the NoteActions component to accept actions as props

--- src/components/Note/NoteActions/index.tsx
+++ src/components/Note/NoteActions/index.tsx
@@ -1,19 +1,14 @@
 import { Archive, RotateCcw, Trash2 } from "lucide-react";
 
-import { useNotesStore } from "@/store/notes";
-
 import * as Types from "./types";
 import * as S from "./styles";
 
 export const NoteActions = (props: Types.NoteActionsType) => {
-  const { note } = props;
-
-  const toggleArchive = useNotesStore((state) => state.toggleArchive);
-  const deleteNote = useNotesStore((state) => state.deleteNote);
+  const { note, onArchiveNote, onDeleteNote } = props;
 
   return (
     <S.Container>
-      <S.ActionButton onClick={() => toggleArchive(note.title)}>
+      <S.ActionButton onClick={() => onArchiveNote(note.title)}>
         {note.isArchived ? (
           <RotateCcw size={20} strokeWidth={1.5} />
         ) : (
@@ -21,7 +16,7 @@ export const NoteActions = (props: Types.NoteActionsType) => {
         )}
         <span>{note.isArchived ? "Restore Note" : "Archive Note"}</span>
       </S.ActionButton>
-      <S.DeleteButton onClick={() => deleteNote(note.title)}>
+      <S.DeleteButton onClick={() => onDeleteNote(note.title)}>
         <Trash2 size={20} strokeWidth={1.5} />
         <span>Delete Note</span>
       </S.DeleteButton>
--- src/components/Note/NoteActions/types.ts
+++ src/components/Note/NoteActions/types.ts
@@ -2,4 +2,6 @@ import { NoteType } from "@/types/note";
 
 export type NoteActionsType = {
   note: NoteType;
+  onArchiveNote: (title: string) => void;
+  onDeleteNote: (title: string) => void;
 };
