ea888798c5c417a5a3d1450e587ee9f359924c62
Rafael Pereira <orafaeldev@gmail.com>
2025-12-21 16:17:10 -0300

    feat: add the LoadingApplication component

---
+++ src/components/LoadingApplication/index.tsx
@@ -0,0 +1,11 @@
+import { LogoIcon } from "@/components/Icons";
+
+import * as S from "./styles";
+
+export const LoadingApplication = () => (
+  <S.Wrapper>
+    <S.LogoWrapper>
+      <LogoIcon />
+    </S.LogoWrapper>
+  </S.Wrapper>
+);
---
+++ src/components/LoadingApplication/styles.ts
@@ -0,0 +1,55 @@
+import styled, { keyframes } from "styled-components";
+
+const animateLogo = keyframes`
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.035);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+`;
+
+export const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  overflow: hidden;
+
+  width: 100vw;
+  height: 100dvh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: var(--color-neutral-100);
+
+  z-index: 999;
+`;
+
+export const LogoWrapper = styled.div`
+  min-width: 250px;
+  min-height: 250px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & .logo-name {
+    fill: var(--color-base);
+  }
+
+  & :first-child {
+    overflow: visible;
+
+    animation: ${animateLogo} 0.8s infinite;
+  }
+`;
