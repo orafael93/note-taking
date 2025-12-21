ad98908e094e5a71e375a049922678a1f6850240
Rafael Pereira <orafaeldev@gmail.com>
2025-12-20 21:43:50 -0300

    feat: add the Tag page

---
+++ src/pages/Tags/index.tsx
@@ -0,0 +1,167 @@
+import { Plus, Settings } from "lucide-react";
+import { useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { NoteCard } from "@/components/Note/NoteCard";
+import { NoteDetail } from "@/components/NoteDetail";
+import { CreateNote } from "@/components/CreateNote";
+import { MobileLogo } from "@/components/Logo";
+import { SearchInput } from "@/components/SearchInput";
+import { useDebounce } from "@/hooks/useDebounce";
+import { capitalizeTag } from "@/utils";
+import { useNotesStore } from "@/store/notes";
+import { useNotesFilter } from "@/hooks/useNotesFilter";
+
+import * as S from "./styles";
+
+export const Tags = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const tagName = pathname.split("/")[2];
+
+  const { notes, onNotesFilter } = useNotesFilter({
+    filterBy: "tags",
+    tag: tagName,
+  });
+
+  const onUpdateSearchValue = (param: string) => {
+    onNotesFilter(param);
+  };
+
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const debouncedFunction = useDebounce(onUpdateSearchValue, 300);
+
+  const isSearchingNotes = useNotesStore((store) => store.isSearchingNotes);
+  const selectedNoteId = useNotesStore((state) => state.selectedNoteId);
+  const [creatingNewNote, setCreatingNewNote] = useState(false);
+  const onUpdateSelectedNote = useNotesStore(
+    (state) => state.onUpdateSelectedNote
+  );
+
+  const contentWrapperRef = useRef<HTMLDivElement | null>(null);
+
+  const handleSelectNote = (noteId: string | null) => {
+    contentWrapperRef.current?.scrollTo({
+      top: 0,
+    });
+
+    if (creatingNewNote) {
+      setCreatingNewNote(false);
+    }
+
+    onUpdateSelectedNote(noteId);
+  };
+
+  const onCreateNewNote = () => {
+    setCreatingNewNote(true);
+
+    if (selectedNoteId) {
+      handleCloseNote();
+    }
+  };
+
+  const handleCloseNote = () => {
+    onUpdateSelectedNote(null);
+  };
+
+  return (
+    <S.MainContent>
+      <MobileLogo onClick={() => navigate("/")} />
+
+      <S.MainHeaderWrapper>
+        <S.SectionTitleWrapper>
+          <S.NotesTaggedTitle>
+            Notes tagged:{" "}
+            <S.SearchedTag>{capitalizeTag(tagName)}</S.SearchedTag>
+          </S.NotesTaggedTitle>
+        </S.SectionTitleWrapper>
+
+        <S.SearchInputAndSettingsIconWrapper>
+          <SearchInput
+            onUpdateValue={(e) => {
+              debouncedFunction(e);
+            }}
+          />
+
+          <S.SettingsIconWrapper onClick={() => navigate("/settings")}>
+            <Settings
+              size={20}
+              color="var(--color-neutral-500)"
+              style={{ cursor: "pointer" }}
+              aria-label="Settings icon"
+            />
+          </S.SettingsIconWrapper>
+        </S.SearchInputAndSettingsIconWrapper>
+      </S.MainHeaderWrapper>
+
+      <S.ContentWrapper ref={contentWrapperRef}>
+        {(selectedNoteId || creatingNewNote) &&
+        window.innerWidth <= 1024 ? null : (
+          <S.Container ref={containerRef}>
+            <S.CreateButton
+              style={{ marginBottom: "16px" }}
+              onClick={onCreateNewNote}
+            >
+              <Plus size={16} strokeWidth={3} />
+              Create New Note
+            </S.CreateButton>
+
+            <S.NoteTagInfoWrapper>
+              <p>
+                All notes with the <span>"{capitalizeTag(tagName)}"</span> tag
+                are shown here.
+              </p>
+            </S.NoteTagInfoWrapper>
+
+            {isSearchingNotes && (
+              <SearchInput
+                onUpdateValue={(e) => {
+                  debouncedFunction(e);
+                }}
+              />
+            )}
+
+            <S.NotesList>
+              {notes.map((note, index, allNotesArray) => (
+                <NoteCard
+                  key={note.title}
+                  note={note}
+                  isActive={note.title === selectedNoteId}
+                  isLastItem={index === allNotesArray.length - 1}
+                  onClick={() =>
+                    handleSelectNote(
+                      selectedNoteId === note.title ? null : note.title
+                    )
+                  }
+                />
+              ))}
+            </S.NotesList>
+          </S.Container>
+        )}
+
+        {selectedNoteId && (
+          <NoteDetail
+            noteId={selectedNoteId}
+            onBack={handleCloseNote}
+            key={selectedNoteId}
+          />
+        )}
+
+        {creatingNewNote && (
+          <CreateNote
+            containerRef={containerRef}
+            onBack={() => setCreatingNewNote(false)}
+          />
+        )}
+
+        {!creatingNewNote && (
+          <S.CreateButtonMobile onClick={onCreateNewNote}>
+            <Plus color="var(--color-white)" />
+          </S.CreateButtonMobile>
+        )}
+      </S.ContentWrapper>
+    </S.MainContent>
+  );
+};
---
+++ src/pages/Tags/styles.ts
@@ -0,0 +1,269 @@
+import styled from "styled-components";
+
+export const MainContent = styled.main`
+  position: relative;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr;
+
+  background-color: var(--color-neutral-0);
+
+  max-height: calc(100dvh - 81px);
+
+  border-left: 1px solid var(--color-neutral-200);
+
+  @media (max-width: 1024px) {
+    overflow: hidden;
+    height: calc(100dvh - 64px);
+
+    grid-column: 1 / -1;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  padding: 20px;
+
+  scrollbar-color: var(--color-neutral-200) transparent;
+  scrollbar-width: thin;
+
+  overflow-y: hidden;
+
+  &:hover {
+    overflow-y: auto;
+  }
+
+  @media (max-width: 1024px) {
+    overflow-y: auto;
+
+    border-right: none;
+
+    padding: 0;
+    padding-bottom: 30px;
+  }
+`;
+
+export const SectionTitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TitleWrapper = styled.div`
+  padding: 12px;
+
+  @media (max-width: 1024px) {
+    padding: 12px 0;
+  }
+`;
+
+export const Title = styled.h1`
+  ${({ theme }) => theme.typography.presets.heading1}
+
+  color: var(--color-neutral-950);
+`;
+
+export const NotesTaggedTitle = styled.h1`
+  ${({ theme }) => theme.typography.presets.heading1}
+
+  color: var(--color-neutral-400);
+`;
+
+export const SearchedTag = styled.span`
+  color: var(--color-base);
+`;
+
+export const CreateButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+
+  padding: ${({ theme }) => `${theme.spacing[150]} ${theme.spacing[200]}`};
+  background: var(--color-blue-500);
+  border: none;
+
+  border-radius: 8px;
+
+  color: var(--color-white);
+
+  font-weight: bold;
+
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: var(--color-blue-500);
+  }
+
+  &:focus-visible {
+    outline-color: var(--color-white);
+    outline-offset: -3px;
+  }
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const SearchInput = styled.input`
+  ${({ theme }) => theme.typography.presets.body1}
+  background: var(--color-neutral-800);
+  color: var(--color-neutral-100);
+  border: 1px solid var(--color-neutral-700);
+  border-radius: 8px;
+  padding: ${({ theme }) => `${theme.spacing[100]} ${theme.spacing[200]}`};
+  width: 300px;
+
+  &::placeholder {
+    color: var(--color-neutral-500);
+  }
+`;
+
+export const NotesList = styled.div`
+  & > div:last-child {
+    border-bottom: none;
+  }
+`;
+
+export const MainHeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 20px 30px;
+
+  min-height: 81px;
+
+  border-bottom: 2px solid var(--color-neutral-200);
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const AllNotesHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border-bottom: 1px solid var(--color-neutral-800);
+`;
+
+export const SearchInputAndSettingsIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.75rem;
+`;
+
+export const SettingsIconWrapper = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 290px 1fr;
+
+  border-right: 1px solid var(--color-neutral-200);
+
+  overflow-y: scroll;
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  height: 100%;
+
+  @media (max-width: 1024px) {
+    height: calc(100dvh - 128px);
+    border: none;
+
+    grid-template-columns: 1fr;
+
+    padding: 16px;
+  }
+`;
+
+export const CreateButtonMobile = styled.button`
+  position: fixed;
+  right: 35px;
+  bottom: 90px;
+
+  background: var(--color-blue-500);
+
+  border-radius: 50%;
+
+  border: 2px solid transparent;
+
+  display: none;
+
+  &:focus-visible {
+    outline-color: var(--color-white);
+    outline-offset: -3px;
+  }
+
+  &:focus {
+    border: 2px solid var(--color-neutral-100);
+  }
+
+  @media (max-width: 1024px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 64px;
+    height: 64px;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 48px;
+    height: 48px;
+  }
+`;
+
+export const NoNotesToShowWrapper = styled.div`
+  background: var(--color-neutral-100);
+  padding: 8px;
+
+  border-radius: 8px;
+
+  font-size: 13px;
+  font-weight: 500;
+
+  color: var(--color-neutral-950);
+
+  & p {
+    display: inline;
+  }
+`;
+
+export const NoteTagInfoWrapper = styled.div`
+  margin: 0 10px;
+
+  border-radius: 8px;
+
+  font-size: 13px;
+  font-weight: 500;
+
+  & p {
+    display: inline;
+    color: var(--color-neutral-500);
+  }
+
+  & span {
+    color: var(--color-base);
+  }
+`;
